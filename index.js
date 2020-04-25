747fe3d74f20a43b2d4f26fa737cdaf9be16cd87
paddycboy <patrick_cauley@hotmail.com>
2020-04-24 23:05:45 -0400

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class TImer {
    constructor (durationInput, startButton, pauseButton) {
       this.durationInput = durationInput;
@@ -9,3 +10,16 @@ class TImer {
       console.log('Time to start the timer!');
    }
 }
+=======
+class TImer {
+   constructor (durationInput, startButton, pauseButton) {
+      this.durationInput = durationInput;
+      this.startButton = startButton;
+      this.pauseButton = pauseButton;
+      this.startButton.addEventListener('click', this.start);
+   }
+   start () {
+      console.log('Time to start the timer!');
+   }
+}
+>>>>>>> 3d04970396a7da55a7fd02b943a60f4533a526cb
