d3528443c8595b6c4fb9d760c5e6eabe5e23a280
paddycboy <patrick_cauley@hotmail.com>
2020-04-24 21:56:27 -0400

    I started the project

---
+++ index.js
@@ -0,0 +1,11 @@
+class TImer {
+   constructor (durationInput, startButton, pauseButton) {
+      this.durationInput = durationInput;
+      this.startButton = startButton;
+      this.pauseButton = pauseButton;
+      this.startButton.addEventListener('click', this.start);
+   }
+   start () {
+      console.log('Time to start the timer!');
+   }
+}
