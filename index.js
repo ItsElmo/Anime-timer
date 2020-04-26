76dcb23e50c1d9ff097910773ced006d60ab484b
paddy256 <Patrickgcauley@gmail.com>
2020-04-25 22:43:23 -0400

    nothing

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 class Timer {
-   constructor (durationInput, startButton, pauseButton) {
-      this.durationInput = durationInput;
-      this.startButton = startButton;
-      this.pauseButton = pauseButton;
-
-      this.startButton.addEventListener('click', this.start);
-   }
-   start () {
-      console.log('Time to start the timer!');
-   }
+	constructor(durationInput, startButton, pauseButton) {
+this.durationInput = durationInput;
+this.startButton = 
+	}
 }
-const durationInput = document.querySelector('#duration');
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
-
-const timer = new Timer(durationInput, startButton, pauseButton);
